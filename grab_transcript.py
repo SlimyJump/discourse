903a9e6ac0581e4a7d12d25b856241cc902e5492
Er <slimyjump@gmail.com>
2026-03-18 18:00:01 -0600

    rss

--- grab_transcript.py
+++ grab_transcript.py
@@ -1,10 +1,25 @@
-from youtube_transcript_api import YouTubeTranscriptApi
+import feedparser
 
-ytt_api = YouTubeTranscriptApi()
+feed = feedparser.parse(
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCXIJgqnII2ZOINSWNOGFThA"
+)
 
-#### rick roll
-#### transcript = ytt_api.fetch("dQw4w9WgXcQ")
-transcript = ytt_api.fetch("o-He1C-fU-s")
+print(f' length of feed results  {len(feed.entries)}')
 
-for entry in transcript[:5]:
-    print(entry.text)
+video_ids = [entry.yt_videoid for entry in feed.entries[:10]]
+
+print(video_ids)
+
+
+############# GET TRANSCRIPT
+
+# from youtube_transcript_api import YouTubeTranscriptApi
+
+# ytt_api = YouTubeTranscriptApi()
+
+# #### rick roll
+# #### transcript = ytt_api.fetch("dQw4w9WgXcQ")
+# transcript = ytt_api.fetch("o-He1C-fU-s")
+
+# for entry in transcript[:5]:
+#     print(entry.text)
